1c7b031d650a6697656a427f2a395b4540c97401
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-05-01 11:51:53 -0500

    chore(configs): remove publish.mjs export

--- configs/src/index.mjs
+++ configs/src/index.mjs
@@ -1,3 +1,2 @@
 export * from './tsup.mjs'
 export * from './versions.mjs'
-export * from './publish.mjs'
--- configs/src/publish.mjs
+++ configs/src/publish.mjs
@@ -1,12 +1,9 @@
-import { argv, file, write } from 'bun'
+import { argv, file, write, $ } from 'bun'
 import { resolve } from 'node:path'
 import { exit } from 'node:process'
+// import { exec } from 'node:child_process'
 import { parseArgs } from 'util'
 import { version, nextTag, packages } from './versions.mjs'
-
-// 1. get flags from command line using Bun
-// 2. update version in each package.json to match version
-// 3. run npm publish --tag ${flag.release} for each package
 
 function _parseFlags(args) {
   return parseArgs({
@@ -24,6 +21,12 @@ function _parseFlags(args) {
   })
 }
 
+function _getReleaseVersion(values) {
+  if (values.next) return `${version}-next`
+  if (values.stable) return version
+  exit(1)
+}
+
 function _getTags(values) {
   if (values.next) {
     return nextTag
@@ -39,28 +42,34 @@ function publish() {
   const release = _getTags(values)
 
   packages.forEach(async (pkg) => {
-    const path = resolve(
-      import.meta.dir,
-      '..',
-      '..',
-      'packages',
-      pkg,
-      'package.json',
-    )
-    const rawFile = file(path)
+    const workspacePath = resolve(import.meta.dir, '..', '..', 'packages', pkg)
+    const packageJsonPath = resolve(workspacePath, 'package.json')
+    const rawFile = file(packageJsonPath)
     const packageJson = await rawFile.json()
-    const json = JSON.stringify({ ...packageJson, version })
+    const json = JSON.stringify(
+      { ...packageJson, version: _getReleaseVersion(values) },
+      null,
+      2,
+    )
 
     // eslint-disable-next-line no-undef
-    console.log('Updating version in', path)
-    write(path, json)
+    console.log('Updating version in', packageJsonPath)
+    write(packageJsonPath, json)
+
+    // eslint-disable-next-line no-undef
+    console.log('Building', pkg, `at ${workspacePath}`)
+    await $`pnpm --filter @cerberus-design/${pkg} build`
+    // exec(`pnpm build`, {
+    //   cwd: workspacePath,
+    // })
 
     // eslint-disable-next-line no-undef
     console.log(`Publishing ${pkg} with tag ${release}`)
-    //     exec(`pnpm publish --tag ${release}`, {
-    //       cwd: `packages
-    // /${pkg}`,
-    //     })
+    await $`cd ${workspacePath} && pnpm publish --tag ${release}`
+
+    // exec(`pnpm publish --tag ${release}`, {
+    //   cwd: `packages/${pkg}`,
+    // })
   })
 }
 
