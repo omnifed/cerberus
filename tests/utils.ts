4cb69fbe606c857b086d5e68acfe8c2ac4756f25
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-05-07 11:33:40 -0500

    test: add theme.test.tsx

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,3 +1,8 @@
+import { configure } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 export const user = userEvent.setup()
+
+export function setupStrictMode() {
+  configure({ reactStrictMode: true })
+}
