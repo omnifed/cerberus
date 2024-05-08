1b7548c7447b263a7fe5100ddc7b678ef98c15a3
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-05-08 12:21:34 -0500

    docs: update nav to use grid

--- docs/app/components/Nav.tsx
+++ docs/app/components/Nav.tsx
@@ -3,7 +3,7 @@
 import { useMemo } from 'react'
 import Link from 'next/link'
 import { css } from '@/styled-system/css'
-import { hstack } from '@/styled-system/patterns'
+import { grid, gridItem, hstack } from '@/styled-system/patterns'
 import navData from '@/app/data/navLinks.json'
 import { LogoGithub } from '@cerberus-design/icons'
 import { useThemeContext } from '@cerberus-design/react'
@@ -18,110 +18,135 @@ export function Nav() {
 
   return (
     <nav
-      className={hstack({
-        flexWrap: 'wrap',
+      className={grid({
+        alignItems: 'center',
+        columns: 3,
+        gridTemplateRows: '1fr 1fr',
         gap: '0',
-        justify: 'center',
         md: {
+          gridTemplateRows: '1fr',
           pxi: '6',
           py: '6',
-          justifyContent: 'space-between',
         },
       })}
     >
-      <div
-        className={css({
-          pxi: '4',
-          py: '6',
-          textAlign: 'center',
-          w: 'full',
-          md: {
-            py: '4',
-            w: 'auto',
-          },
+      <section
+        className={gridItem({
+          gridColumnStart: 1,
+          gridColumnEnd: 2,
         })}
       >
         <Link
           href="/"
           className={css({
+            pxi: '2',
             textStyle: 'body-xl',
           })}
         >
           <strong>Cerberus </strong>
           Design System
         </Link>
-      </div>
+      </section>
 
-      <ul
-        className={hstack({
-          border: '1px solid',
-          borderColor: 'neutral.border.initial',
-          justify: 'space-evenly',
-          w: 'full',
+      <section
+        className={gridItem({
+          gridColumnStart: 1,
+          gridColumnEnd: 4,
+          gridRowStart: 2,
+          gridRowEnd: 3,
           md: {
-            border: 'none',
-            w: 'auto',
+            gridColumnStart: 2,
+            gridColumnEnd: 3,
+            gridRowStart: 'initial',
+            gridRowEnd: 'initial',
           },
-          '& li:nth-child(2)': {
+        })}
+      >
+        <ul
+          className={hstack({
+            border: '1px solid',
             borderColor: 'neutral.border.initial',
-            borderLeft: '1px solid',
-            borderRight: '1px solid',
+            gap: '0',
+            w: 'full',
             md: {
               border: 'none',
+              w: 'auto',
             },
-          },
-        })}
-      >
-        {navData.map((item) => (
-          <li
-            className={css({
-              textAlign: 'center',
-              w: 'calc(100% / 3)',
-            })}
-            key={item.id}
-          >
-            <Link
-              className={css({
-                display: 'inline-block',
-                p: '4',
-                w: 'full',
-              })}
-              href={item.href}
-            >
-              {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-
-      <ul
-        className={hstack({
-          display: 'none',
-          md: {
-            display: 'flex',
-          },
-        })}
-      >
-        <li>
-          <a
-            aria-label="View Github repo"
-            href="https://github.com/omnifed/cerberus"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <LogoGithub aria-hidden size="1.5rem" />
-          </a>
-        </li>
-        <li
-          className={css({
-            h: '1.5rem',
+            '& li:nth-child(2)': {
+              borderColor: 'neutral.border.initial',
+              borderLeft: '1px solid',
+              borderRight: '1px solid',
+              md: {
+                border: 'none',
+              },
+            },
           })}
         >
-          <button aria-label={ariaLabel} onClick={updateMode}>
-            {mode === 'light' ? <AnimatingSunIcon /> : <AnimatingMoonIcon />}
-          </button>
-        </li>
-      </ul>
+          {navData.map((item) => (
+            <li
+              className={css({
+                textAlign: 'center',
+                w: 'calc(100% / 3)',
+              })}
+              key={item.id}
+            >
+              <Link
+                className={css({
+                  display: 'inline-block',
+                  p: '4',
+                  w: 'full',
+                })}
+                href={item.href}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section
+        className={gridItem({
+          gridColumnStart: 3,
+          gridColumnEnd: 4,
+          gridRowStart: 1,
+          gridRowEnd: 2,
+          paddingInlineEnd: '2',
+          justifySelf: 'end',
+          md: {
+            gridColumnStart: 3,
+            gridColumnEnd: 4,
+            gridRowStart: 1,
+            gridRowEnd: 2,
+          },
+        })}
+      >
+        <ul
+          className={hstack({
+            gap: '4',
+          })}
+        >
+          <li>
+            <a
+              aria-label="View Github repo"
+              href="https://github.com/omnifed/cerberus"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <LogoGithub aria-hidden size="1.5rem" />
+            </a>
+          </li>
+          <li
+            className={css({
+              h: '1.5rem',
+            })}
+          >
+            <button aria-label={ariaLabel} onClick={updateMode}>
+              {mode === 'light' ? <AnimatingSunIcon /> : <AnimatingMoonIcon />}
+            </button>
+          </li>
+        </ul>
+      </section>
     </nav>
   )
 }
