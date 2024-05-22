46a78b6b7ff289da9be5c23d50c16de074fb11db
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-05-22 14:45:14 -0500

    docs: add animation previews

---
+++ docs/app/preset/animation/components/animation-preview.tsx
@@ -0,0 +1,44 @@
+import { vstack } from '@/styled-system/patterns'
+import { circle, square } from '@cerberus-design/styled-system/patterns'
+
+export function PingPreview() {
+  return (
+    <span
+      className={vstack({
+        justify: 'center',
+        position: 'relative',
+      })}
+    >
+      <span
+        className={circle({
+          bgColor: 'success.border.initial',
+          position: 'absolute',
+          size: '2',
+          zIndex: 'decorator',
+        })}
+      />
+      <span
+        className={circle({
+          animation: 'ping 2s infinite',
+          bgColor: 'success.surface.initial',
+          position: 'absolute',
+          size: '2',
+          zIndex: 'base',
+        })}
+      />
+    </span>
+  )
+}
+
+export function PulsePreview() {
+  return (
+    <div
+      className={square({
+        animation: 'pulse 2s infinite',
+        bgColor: 'neutral.surface.300',
+        rounded: 'md',
+        size: '10rem',
+      })}
+    />
+  )
+}
