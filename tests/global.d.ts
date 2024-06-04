c759b9e482723e2a1e7bbe85fe8d4848cd5a67e3
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-06-04 11:22:29 -0500

    chore: update es settings

---
+++ tests/global.d.ts
@@ -0,0 +1,19 @@
+declare module '@cerberus/styled-system/css' {
+  export * from '@cerberus-design/styled-system/css'
+}
+
+declare module '@cerberus/styled-system/recipes' {
+  export * from '@cerberus-design/styled-system/recipes'
+}
+
+declare module '@cerberus/styled-system/types' {
+  export * from '@cerberus-design/styled-system/types'
+}
+
+declare module '@cerberus/styled-system/utils' {
+  export * from '@cerberus-design/styled-system/utils'
+}
+
+declare module '@cerberus/styled-system/patterns' {
+  export * from '@cerberus-design/styled-system/patterns'
+}
