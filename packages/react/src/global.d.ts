c9b4bae77eafd8efb683c7347acd591b1c8b3178
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-06-05 14:42:44 -0500

    docs: update global.d.ts

--- packages/react/src/global.d.ts
+++ packages/react/src/global.d.ts
@@ -1,8 +1,8 @@
-declare module '@cerberus/styled-system/css' {
+declare module '@cerberus/styled-system/css/index.mjs' {
   export * from '@cerberus-design/styled-system/css'
 }
 
-declare module '@cerberus/styled-system/recipes' {
+declare module '@cerberus/styled-system/recipes/index.mjs' {
   export * from '@cerberus-design/styled-system/recipes'
 }
 
@@ -10,10 +10,10 @@ declare module '@cerberus/styled-system/types' {
   export * from '@cerberus-design/styled-system/types'
 }
 
-declare module '@cerberus/styled-system/utils' {
+declare module '@cerberus/styled-system/utils/index.mjs' {
   export * from '@cerberus-design/styled-system/utils'
 }
 
-declare module '@cerberus/styled-system/patterns' {
+declare module '@cerberus/styled-system/patterns/index.mjs' {
   export * from '@cerberus-design/styled-system/patterns'
 }
