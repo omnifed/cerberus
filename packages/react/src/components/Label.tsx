e3b521e2fb26484c5a2118b4777ab7c1262d3d55
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-06-10 12:46:10 -0500

    feat(react): add JSDOC to Label component

--- packages/react/src/components/Label.tsx
+++ packages/react/src/components/Label.tsx
@@ -2,11 +2,25 @@ import type { HTMLAttributes } from 'react'
 import { label } from '@cerberus/styled-system/recipes'
 import { cx } from '@cerberus/styled-system/css'
 
+/**
+ * This module contains the Label component.
+ * @module
+ */
+
 export interface LabelProps extends HTMLAttributes<HTMLLabelElement> {
   htmlFor: string
   hidden?: boolean
 }
 
+/**
+ * A screen ready friendly label component.
+ * @definition [ARIA Forms](https://www.a11yproject.com/checklist/#forms)
+ * @definition [Label docs](https://cerberus.digitalu.design/react/label)
+ * @example
+ * ```tsx
+ * <Label htmlFor="test">Test Label</Label>
+ * ```
+ */
 export function Label(props: LabelProps) {
   const { hidden, ...nativeProps } = props
   const usage = hidden ? 'hidden' : 'visible'
