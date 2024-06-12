9b6c290c1e84a3a2770f9fe1d83545a884718d32
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-06-12 16:38:32 -0500

    chore: add JSDOC info to TabPanel

--- packages/react/src/components/TabPanel.tsx
+++ packages/react/src/components/TabPanel.tsx
@@ -5,11 +5,27 @@ import { useMemo, type HTMLAttributes } from 'react'
 import { useTabsContext } from '../context/tabs'
 import { Show } from './Show'
 
+/**
+ * This module provides a TabPanel component.
+ * @module
+ */
+
 export interface TabPanelProps extends HTMLAttributes<HTMLDivElement> {
   id: string
   tab: string
 }
 
+/**
+ * The TabPanel component provides a panel element to be used in a Tabs provider.
+ * @param id - the id of the tab panel which is used for the Tab's control prop
+ * @param tab - the id of the tab that will be tracked as the active tab
+ * @example
+ * ```tsx
+ * <TabPanel id="panel:overview" tab="overview">
+ *   Overview content
+ * </TabPanel>
+ * ```
+ */
 export function TabPanel(props: TabPanelProps) {
   const { tab, ...nativeProps } = props
   const { active } = useTabsContext()
