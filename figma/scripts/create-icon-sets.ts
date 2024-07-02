d718fd4f4f216685495347ba4d71e88ce6217795
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-07-02 10:40:43 -0500

    feat(figma): init create-icons.ts

---
+++ figma/scripts/create-icon-sets.ts
@@ -0,0 +1,26 @@
+import { client } from '@figma/code-connect'
+
+async function getIcons() {
+  try {
+    const components = await client.getComponents(
+      'https://www.figma.com/design/ducwqOCxoxcWc3ReV3FYd8/Digital-University-Component-Library?node-id=8828-2234&t=gMSERYVOSi9I3gOh-0',
+    )
+
+    console.log({
+      components,
+      properties: components.map(
+        ({ componentPropertyDefinitions }) => componentPropertyDefinitions,
+      ),
+    })
+
+    // STEP 1: Get all the icons (should just be components)
+
+    // STEP 2: Create a new file for each icon [name].figma.tsx in the src/icons folder (using Bun)
+
+    // Step 3. The contents should match the arrow-right.figma.tsx file
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getIcons()
