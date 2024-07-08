4d08139bff4f03a1e4681cbca707836e204d5f27
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-07-08 14:54:17 -0500

    build(react): update tsup config

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'tsup'
 import { modernConfig, legacyConfig } from '@cerberus-design/configs'
+import { devDependencies, peerDependencies } from './package.json'
+
+const externals = [
+  ...Object.keys(devDependencies),
+  ...Object.keys(peerDependencies),
+  'react',
+  'react-dom',
+  '@cerberus/styled-system/css',
+  '@cerberus/styled-system/patterns',
+  '@cerberus/styled-system/recipes',
+]
 
 export default defineConfig([
-  modernConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'] }),
-  legacyConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'] }),
+  modernConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'], externals }),
+  legacyConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'], externals }),
 ])
