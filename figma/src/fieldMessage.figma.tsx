1349e0ebdfb5aff23fc6e9a8d888cd698f4886d3
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-07-10 10:29:02 -0500

    refactor(figma): update connect link in fieldMessage.figma.tsx

--- figma/src/fieldMessage.figma.tsx
+++ figma/src/fieldMessage.figma.tsx
@@ -2,13 +2,13 @@ import figma from '@figma/code-connect'
 import { FieldMessage } from '@cerberus-design/react'
 
 const SELECTION =
-  'https://www.figma.com/design/ducwqOCxoxcWc3ReV3FYd8/Digital-University-Component-Library?node-id=9225-4248&m=dev'
+  'https://www.figma.com/design/ducwqOCxoxcWc3ReV3FYd8/Digital-University-Component-Library?node-id=9266-1663&m=dev'
 
 const imports = ["import { FieldMessage } from '@cerberus/react'"]
 
 const props = {
   invalid: figma.boolean('invalid'),
-  text: figma.textContent('text'),
+  text: figma.textContent('Text'),
 }
 
 figma.connect(FieldMessage, SELECTION, {
