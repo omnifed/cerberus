60a7d706bc6e9c2b726dc35a0b7e6fe46eb5d359
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-07-19 14:20:02 -0500

    docs: update metadata

--- docs/app/react/layout.tsx
+++ docs/app/react/layout.tsx
@@ -2,8 +2,13 @@ import type { PropsWithChildren } from 'react'
 import { PageLayout, PageSideNav } from '../components/PageLayout'
 import SideNav, { type NavList } from '../components/SideNav'
 import sideNavData from './side-nav.json'
+import type { Metadata } from 'next/types'
 
 interface ReactProps {}
+
+export const metadata: Metadata = {
+  title: 'Cerberus Design System | React',
+}
 
 export default function ReactLayout(props: PropsWithChildren<ReactProps>) {
   return (
---
+++ docs/app/shared-metadata.ts
@@ -0,0 +1,10 @@
+export const description =
+  'Guarding the brand integrity of the Digital University realm by creating a flexible and extensible design system.'
+
+export const openGraph = {
+  title: 'Cerberus Design System',
+  description:
+    'Guarding the brand integrity of the Digital University realm by creating a flexible and extensible design system.',
+  type: 'website',
+  url: 'https://cerberus.design',
+}
