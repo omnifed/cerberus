4f8b7eb01a275a91a62ed7f5bb12a02a7cf76386
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-08-29 11:06:19 -0500

    chore: up next & metadata info

--- docs/app/shared-metadata.ts
+++ docs/app/shared-metadata.ts
@@ -1,5 +1,10 @@
-export const description =
+const description =
   'Guarding the brand integrity of the Digital University realm by creating a flexible and extensible design system.'
+
+export const base = {
+  metadataBase: new URL('https://cerberus.digitalu.design/'),
+  description,
+}
 
 export const openGraph = {
   title: 'Cerberus Design System',
