ad62f5763070b2c89386710ef6cace19d63e62ee
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-09-04 12:01:45 -0500

    test: add select.test.ts

---
+++ tests/panda-preset/recipes/slots/select.test.ts
@@ -0,0 +1,111 @@
+import { describe, test, expect } from 'bun:test'
+import { slotRecipes } from '@cerberus-design/panda-preset'
+
+describe('select recipe', () => {
+  const { select } = slotRecipes
+
+  test('should be exported', () => {
+    expect(select).toBeDefined()
+  })
+
+  test('should have a base style', () => {
+    expect(select.base?.root).toMatchObject({
+      h: '3.7rem',
+      position: 'relative',
+      py: '1',
+      w: 'full',
+    })
+  })
+
+  test('should have a input style', () => {
+    expect(select.base?.input).toMatchObject({
+      border: '1px solid',
+      borderColor: 'action.border.initial',
+      color: 'page.text.initial',
+      h: 'full',
+      pxi: '4',
+      rounded: 'sm',
+      transitionProperty: 'border-color',
+      transitionDuration: '200ms',
+      transitionTimingFunction: 'ease-in-out',
+      w: 'full',
+      _focusVisible: {
+        boxShadow: 'none',
+        outline: '3px solid',
+        outlineColor: 'action.border.focus',
+        outlineOffset: '2px',
+      },
+      _disabled: {
+        cursor: 'not-allowed',
+        opacity: '0.5',
+      },
+      _readOnly: {
+        cursor: 'default',
+      },
+      _userInvalid: {
+        borderColor: 'danger.border.initial',
+      },
+      _placeholderShown: {
+        color: 'page.text.100',
+      },
+      _startIcon: {
+        display: 'inline-block',
+        paddingInlineStart: '7',
+      },
+    })
+  })
+
+  test('should have a iconStack style', () => {
+    expect(select.base?.iconStack).toMatchObject({
+      position: 'absolute',
+      right: '4',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      zIndex: 'decorator',
+    })
+  })
+
+  test('should have a stateIcon style', () => {
+    expect(select.base?.stateIcon).toMatchObject({
+      _invalid: {
+        color: 'danger.text.200',
+      },
+    })
+  })
+
+  test('should have a size variant', () => {
+    expect(select.variants?.size).toMatchObject({
+      sm: {
+        root: {
+          md: {
+            h: '2rem',
+          },
+        },
+        input: {
+          fontSize: 'sm',
+        },
+      },
+      md: {
+        root: {
+          md: {
+            h: '2.5rem',
+          },
+        },
+        input: {
+          fontSize: 'sm',
+        },
+      },
+      lg: {
+        root: {
+          h: '3rem',
+        },
+      },
+    })
+  })
+
+  test('should have a default variant', () => {
+    expect(select.defaultVariants).toMatchObject({
+      size: 'lg',
+    })
+  })
+})
