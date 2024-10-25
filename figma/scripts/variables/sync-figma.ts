7c5587d7846b8473d104be733ba1b803e8019ff4
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-10-25 09:08:47 -0500

    feat(preset): sync Figma text styles

--- figma/scripts/variables/sync-figma.ts
+++ figma/scripts/variables/sync-figma.ts
@@ -4,6 +4,7 @@ import { resolve } from 'node:path'
 import FigmaApi from './figma-api'
 import { green } from './utils'
 import { tokenFilesFromLocalVariables } from './token-export'
+import { textStyleNodes } from './text-styles'
 
 async function main() {
   if (!process.env.FIGMA_ACCESS_TOKEN) {
@@ -11,7 +12,13 @@ async function main() {
   }
 
   const api = new FigmaApi(process.env.FIGMA_ACCESS_TOKEN)
-  const localVariables = await api.getLocalVariables()
+  // const localVariables = await api.getLocalVariables()
+  // const textStyles = await api.getTextStyles(textStyleNodes)
+
+  const [localVariables, textStyles] = await Promise.all([
+    api.getLocalVariables(),
+    api.getTextStyles(textStyleNodes),
+  ])
 
   const tokenDirPath = resolve(
     import.meta.dir,
@@ -26,6 +33,7 @@ async function main() {
   )
   const tokensFiles = tokenFilesFromLocalVariables(localVariables)
 
+  // Write variable files
   Object.entries(tokensFiles).forEach(async ([fileName, fileContent]) => {
     await write(
       resolve(tokenDirPath, fileName),
@@ -33,6 +41,12 @@ async function main() {
     )
     console.log(`Wrote ${fileName}`)
   })
+
+  // Write text styles file
+  await write(
+    resolve(tokenDirPath, 'text-styles.json'),
+    JSON.stringify(textStyles.nodes, null, 2),
+  )
 
   console.log(
     green(`✅ Tokens files have been written to the ${tokenDirPath} directory`),
