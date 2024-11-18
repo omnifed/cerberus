064393fd7454befce7942a1f6214b901656ab7b5
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-11-18 12:37:08 -0600

    test: add tooltip tests

---
+++ tests/react/components/tooltip.test.tsx
@@ -0,0 +1,34 @@
+import { describe, test, expect, afterEach } from 'bun:test'
+import { cleanup, render, screen, waitFor } from '@testing-library/react'
+import { Tooltip } from '@cerberus-design/react'
+import { setupStrictMode } from '@/utils'
+import userEvent from '@testing-library/user-event'
+
+describe('Tooltip', () => {
+  setupStrictMode()
+  afterEach(cleanup)
+
+  test('should render a tooltip element', async () => {
+    render(<Tooltip content="It works">trigger</Tooltip>)
+    await userEvent.hover(screen.getByText(/trigger/i))
+    await waitFor(() => {
+      expect(screen.getByText(/it works/i)).toBeTruthy()
+    })
+  })
+
+  test('should render a tooltip with a custom position', async () => {
+    render(
+      <Tooltip content="It works" position="bottom">
+        trigger
+      </Tooltip>,
+    )
+    await userEvent.hover(screen.getByText(/trigger/i))
+    await waitFor(() => {
+      expect(
+        (screen.getByText(/it works/i) as HTMLElement).getAttribute(
+          'data-placement',
+        ),
+      ).toBe('bottom')
+    })
+  })
+})
