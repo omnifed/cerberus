3a7ce4a1ae7ad49997f056dae19ca1fa2dcc64c9
@casey_baggz_omni <casey.baggz@omnifederal.com>
2024-12-06 14:38:09 -0600

    docs: add date-picker docs

--- docs/app/react/date-picker/components/static-preview.tsx
+++ docs/app/react/date-picker/components/static-preview.tsx
@@ -1,9 +1,4 @@
 import {
-  Accordion,
-  AccordionItem,
-  AccordionItemTrigger,
-  AccordionItemContent,
-  AccordionItemIndicator,
   DatePicker,
   DatePickerLabel,
   DatePickerInput,
@@ -12,7 +7,6 @@ import {
   RangePickerInput,
 } from '@cerberus-design/react'
 import { Box } from '@cerberus-design/styled-system/jsx'
-import { css } from '@cerberus-design/styled-system/css'
 
 export function StaticPreview() {
   return (
@@ -38,43 +32,6 @@ export function RangePreview() {
           <DatePickerCalendar />
         </DatePicker>
       </Field>
-    </Box>
-  )
-}
-
-export function CustomPreview() {
-  return (
-    <Box w="2/3">
-      <Accordion>
-        <AccordionItem
-          className={css({
-            bgColor: 'black',
-            borderColor: 'yellow',
-          })}
-          value="one"
-        >
-          <AccordionItemTrigger
-            className={css({
-              color: 'yellow',
-            })}
-          >
-            Wu-Tang Clan
-            <AccordionItemIndicator />
-          </AccordionItemTrigger>
-          <AccordionItemContent
-            className={css({
-              color: 'white',
-              paddingInline: 'md',
-              textStyle: 'body-md',
-            })}
-          >
-            The rugged, raw, and unapologetic Wu-Tang Clan is a hip-hop group
-            that has been around since 1992. The group is known for its
-            distinctive sound and lyrics that often touch on themes of urban
-            life, crime, and the struggles of the inner city New York.
-          </AccordionItemContent>
-        </AccordionItem>
-      </Accordion>
     </Box>
   )
 }
