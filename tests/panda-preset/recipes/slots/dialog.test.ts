25a07c3f95e903585b034a869877e2f94c1960be
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-01-07 09:39:12 -0600

    test: add dialog.test.ts

---
+++ tests/panda-preset/recipes/slots/dialog.test.ts
@@ -0,0 +1,130 @@
+import { describe, test, expect } from 'bun:test'
+import { slotRecipes } from '@cerberus-design/panda-preset'
+
+describe('dialog recipe', () => {
+  const { dialog } = slotRecipes
+  const modalMinH = '18rem'
+
+  test('should create JSX aliases', () => {
+    expect(dialog.jsx).toEqual([
+      'DialogProvider',
+      'Dialog',
+      'DialogTrigger',
+      'DialogCloseTrigger',
+      'DialogHeading',
+      'DialogDescription',
+      'ConfirmModal',
+      'PromptModal',
+      'CTAModal',
+    ])
+  })
+
+  test('should have base styles', () => {
+    expect(dialog).toHaveProperty('base', {
+      backdrop: {
+        animationFillMode: 'forwards',
+        bgColor: 'page.backdrop.initial',
+        backdropFilter: 'blur(2px)',
+        backdropBlur: 'sm',
+        h: '100vh',
+        left: '0',
+        opacity: '0',
+        position: 'fixed',
+        top: '0',
+        w: '100vw',
+        zIndex: 'overlay',
+        _open: {
+          animationStyle: 'emphasized-fade-in',
+        },
+        _closed: {
+          animationStyle: 'emphasized-fade-out',
+        },
+      },
+      positioner: {
+        alignItems: 'center',
+        display: 'flex',
+        h: '100dvh',
+        justifyContent: 'center',
+        left: '0',
+        overflow: 'auto',
+        position: 'fixed',
+        top: '0',
+        w: '100vw',
+        zIndex: 'modal',
+      },
+      content: {
+        animationFillMode: 'forwards',
+        bgColor: 'page.surface.100',
+        padding: 'xl',
+        opacity: 0,
+        overflowY: 'auto',
+        position: 'relative',
+        rounded: 'md',
+        shadow: 'lg',
+        _open: {
+          animationStyle: 'emphasized-slide-in',
+        },
+        _closed: {
+          animationStyle: 'emphasized-slide-out',
+        },
+      },
+      title: {
+        color: 'page.text.initial',
+        textStyle: 'heading-lg',
+      },
+      description: {
+        color: 'page.text.initial',
+        textStyle: 'body-md',
+      },
+      closeTrigger: {
+        position: 'absolute',
+        right: 'md',
+        top: 'md',
+        zIndex: 'decorator',
+      },
+    })
+  })
+
+  test('should have variants', () => {
+    expect(dialog).toHaveProperty('variants', {
+      size: {
+        xs: {
+          content: {
+            minH: modalMinH,
+            w: '26rem',
+          },
+        },
+        sm: {
+          content: {
+            minH: modalMinH,
+            w: '35rem',
+          },
+        },
+        md: {
+          content: {
+            minH: modalMinH,
+            w: '40rem',
+          },
+        },
+        lg: {
+          content: {
+            minH: modalMinH,
+            w: '54rem',
+          },
+        },
+        full: {
+          content: {
+            minH: '40rem',
+            w: '90dvw',
+          },
+        },
+      },
+    })
+  })
+
+  test('should have default variants', () => {
+    expect(dialog).toHaveProperty('defaultVariants', {
+      size: 'md',
+    })
+  })
+})
