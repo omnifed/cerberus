2d1a2f297513a44801e3ead9ae00f92bc10aed7e
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-01-16 16:20:56 -0600

    fix(react): strictly type button/parts.ts

--- packages/react/src/components/button/parts.ts
+++ packages/react/src/components/button/parts.ts
@@ -1,9 +1,15 @@
+import type { ElementType } from 'react'
 import { Button, ButtonIcon } from './button'
 
 /**
  * This module contains the parts of the Button component.
  * @module 'button/parts'
  */
+
+interface ButtonPartsValue {
+  Root: ElementType
+  Icon: ElementType
+}
 
 /**
  * An Object containing the parts of the Button component. For users that
@@ -14,7 +20,7 @@ import { Button, ButtonIcon } from './button'
  * When using object component syntax, you import the ButtonParts object and
  * the entire family of components vs. only what you use.
  */
-export const ButtonParts = {
+export const ButtonParts: ButtonPartsValue = {
   Root: Button,
   Icon: ButtonIcon,
 }
