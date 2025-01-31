607af7587a4147f8555a6374bae38485c9c2a0b9
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-01-31 12:23:20 -0600

    test: add fieldset tests

---
+++ tests/panda-preset/recipes/slots/fieldset.test.ts
@@ -0,0 +1,92 @@
+import { describe, test, expect } from 'bun:test'
+import { slotRecipes } from '@cerberus-design/panda-preset'
+
+describe('fieldset recipe', () => {
+  const { fieldset } = slotRecipes
+
+  test('should be exported', () => {
+    expect(fieldset).toBeDefined()
+  })
+
+  test('should support jsx components', () => {
+    expect(fieldset.jsx).toEqual([
+      'FieldsetRoot',
+      'FieldsetLegend',
+      'FieldsetHelperText',
+      'FieldsetErrorText',
+      'Fieldset',
+    ])
+  })
+
+  test('should support base.root styles', () => {
+    expect(fieldset.base?.root).toMatchObject({
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 'sm',
+      width: 'full',
+    })
+  })
+
+  test('should support base.legend styles', () => {
+    expect(fieldset.base?.legend).toMatchObject({
+      _disabled: {
+        cursor: 'not-allowed',
+        opacity: '0.5',
+        color: 'page.text.100',
+      },
+    })
+  })
+
+  test('should support base.errorText styles', () => {
+    expect(fieldset.base?.errorText).toMatchObject({
+      color: 'danger.text.initial',
+      textStyle: 'label-sm',
+    })
+  })
+
+  test('should support variants.usage.fieldGroup', () => {
+    expect(fieldset.variants?.usage?.fieldGroup).toMatchObject({
+      legend: {
+        alignItems: 'center',
+        display: 'inline-flex',
+        color: 'page.text.initial',
+        justifyContent: 'space-between',
+        textStyle: 'label-sm',
+        w: 'full',
+        _disabled: {
+          color: 'page.text.100',
+        },
+        '& > [data-part=required-indicator]': {
+          display: 'inline-block',
+          color: 'page.text.100',
+          textStyle: 'label-sm',
+        },
+      },
+      helperText: {
+        color: 'page.text.initial',
+        textStyle: 'body-xs',
+      },
+    })
+  })
+
+  test('should support variants.usage.formSection', () => {
+    expect(fieldset.variants?.usage?.formSection).toMatchObject({
+      legend: {
+        color: 'page.text.200',
+        textStyle: 'heading-sm',
+      },
+      helperText: {
+        color: 'page.text.200',
+        paddingBlockStart: 'xs',
+        textStyle: 'body-md',
+      },
+    })
+  })
+
+  test('should support defaultVariants.usage', () => {
+    const defaultVariants = fieldset.defaultVariants as {
+      usage: 'fieldGroup' | 'formSection'
+    }
+    expect(defaultVariants.usage).toEqual('fieldGroup')
+  })
+})
--- tests/react/components/fieldset.test.tsx
+++ tests/react/components/fieldset.test.tsx
@@ -2,57 +2,88 @@ import { describe, test, expect, afterEach } from 'bun:test'
 import { cleanup, render, screen } from '@testing-library/react'
 import {
   Field,
-  FieldsetLabel,
+  Input,
   Radio,
   RadioGroup,
   Fieldset,
-  Legend,
+  FieldsetParts,
+  CerberusProvider,
 } from '@cerberus-design/react'
-import { setupStrictMode } from '@/utils'
+import { makeConfig, setupStrictMode } from '@/utils'
 
-describe('Fieldset & Legend', () => {
+describe('Fieldset & FieldsetParts', () => {
   setupStrictMode()
   afterEach(cleanup)
 
-  test('should render a fieldset with a legend', () => {
-    render(
-      <Field>
-        <Fieldset name="cats">
-          <Legend>Do you like cats?</Legend>
-          <RadioGroup>
-            <Radio id="yes" value="yes">
-              <FieldsetLabel htmlFor="yes">Yes</FieldsetLabel>
-            </Radio>
-            <Radio id="no" value="no">
-              <FieldsetLabel htmlFor="no">No</FieldsetLabel>
-            </Radio>
-          </RadioGroup>
-        </Fieldset>
-      </Field>,
+  const config = makeConfig()
+
+  function TestFormGroup(props: { invalid?: boolean }) {
+    return (
+      <Fieldset
+        legend="test legend"
+        helperText="Helper text"
+        errorText="Error text"
+        usage="formSection"
+        {...props}
+      >
+        <Field label="test label">
+          <Input />
+        </Field>
+      </Fieldset>
     )
-    expect(screen.getByText(/do you like cats/i)).toBeTruthy()
-    expect(screen.getByLabelText(/yes/i)).toBeTruthy()
-    expect(screen.getByLabelText(/no/i)).toBeTruthy()
+  }
+
+  test('should render a fieldset for a form section', () => {
+    render(
+      <CerberusProvider config={config}>
+        <TestFormGroup />
+      </CerberusProvider>,
+    )
+    expect(screen.getByText('test legend')).toBeTruthy()
+    expect(screen.getByText('Helper text')).toBeTruthy()
+    expect(screen.getByText('test label')).toBeTruthy()
   })
 
-  test('should render a fieldset with a legend and a disabled radio', () => {
+  test('should render a fieldset for a form section with error', () => {
     render(
-      <Field disabled>
-        <Fieldset name="cats">
-          <Legend>Do you like disabled cats?</Legend>
-          <RadioGroup>
-            <Radio id="yes" value="yes">
-              <FieldsetLabel htmlFor="yes">Yes</FieldsetLabel>
-            </Radio>
-            <Radio id="no" value="no">
-              <FieldsetLabel htmlFor="no">No</FieldsetLabel>
-            </Radio>
+      <CerberusProvider config={config}>
+        <TestFormGroup invalid />
+      </CerberusProvider>,
+    )
+    expect(screen.getByText('Error text')).toBeTruthy()
+  })
+
+  test('should render a fieldset for a field group', () => {
+    render(
+      <CerberusProvider config={config}>
+        <Fieldset legend="test legend">
+          <RadioGroup name="test group" defaultValue="1">
+            <Radio value="1">one</Radio>
+            <Radio value="2">two</Radio>
           </RadioGroup>
         </Fieldset>
-      </Field>,
+      </CerberusProvider>,
     )
-    expect(
-      screen.getAllByRole('group')[1].attributes.getNamedItem('disabled'),
-    ).toBeTruthy()
+    expect(screen.getByText('test legend')).toBeTruthy()
+    expect(screen.getByText('one')).toBeTruthy()
+    expect(screen.getByText('two')).toBeTruthy()
+  })
+
+  test('should render Fieldset parts', () => {
+    render(
+      <CerberusProvider config={config}>
+        <FieldsetParts.Root>
+          <FieldsetParts.Legend>test legend</FieldsetParts.Legend>
+          <FieldsetParts.HelperText>Helper text</FieldsetParts.HelperText>
+          <Field label="test label">
+            <Input />
+          </Field>
+          <FieldsetParts.ErrorText>Error text</FieldsetParts.ErrorText>
+        </FieldsetParts.Root>
+      </CerberusProvider>,
+    )
+    expect(screen.getByText('test legend')).toBeTruthy()
+    expect(screen.getByText('Helper text')).toBeTruthy()
+    expect(screen.getByText('test label')).toBeTruthy()
   })
 })
