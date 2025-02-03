7af5e155051a2fe2a011c14207b7382cf2c9c247
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-02-03 10:02:59 -0600

    docs: update show demo

--- docs/app/react/show/components/show-preview.tsx
+++ docs/app/react/show/components/show-preview.tsx
@@ -1,33 +1,36 @@
 'use client'
 
-import { Logout } from '@carbon/icons-react'
-import { Button, Show } from '@cerberus-design/react'
+import { Button, Show, ToggleParts, Text } from '@cerberus-design/react'
+import { VStack } from '@cerberus-design/styled-system/jsx'
 import { useState } from 'react'
 
 export default function ShowPreview() {
   const [authenticated, setAuthenticated] = useState<boolean>(false)
 
-  function handleAuthenticate() {
-    setAuthenticated(true)
-  }
-
-  function handleUnauthenticate() {
-    setAuthenticated(false)
+  function handleChange(state: boolean) {
+    setAuthenticated(state)
   }
 
   return (
-    <Show
-      when={authenticated}
-      fallback={
-        <Button onClick={handleAuthenticate} type="button">
-          Sign in
+    <VStack w="1/2">
+      <ToggleParts.Root
+        onPressedChange={handleChange}
+        pressed={authenticated}
+        asChild
+      >
+        <Button type="button">
+          <ToggleParts.Indicator fallback={<>Sign in</>}>
+            Sign out
+          </ToggleParts.Indicator>
         </Button>
-      }
-    >
-      <Button onClick={handleUnauthenticate} type="button">
-        Sign out
-        <Logout />
-      </Button>
-    </Show>
+      </ToggleParts.Root>
+
+      <Show
+        when={authenticated}
+        fallback={<Text>You are not allowed to enter!</Text>}
+      >
+        <Text>You are authenticated and can enter the gates of Hades!</Text>
+      </Show>
+    </VStack>
   )
 }
