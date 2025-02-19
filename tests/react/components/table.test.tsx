ee32d6dc68128796e9daebbdac928626b6565fd5
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-02-19 11:38:28 -0600

    test: add table tests

--- tests/react/components/table.test.tsx
+++ tests/react/components/table.test.tsx
@@ -1,6 +1,6 @@
 import { describe, test, expect, afterEach, jest } from 'bun:test'
 import { cleanup, render, screen } from '@testing-library/react'
-import { Table, Tbody, Td, Th, Thead, Tr } from '@cerberus-design/react'
+import { Table, TableParts } from '@cerberus-design/react'
 import { setupStrictMode } from '@/utils'
 import userEvent from '@testing-library/user-event'
 
@@ -10,32 +10,32 @@ describe('Table', () => {
 
   test('should render a table element', () => {
     render(
-      <Table caption="Basic table">
-        <Thead>
-          <Tr>
-            <Th>Header 1</Th>
-            <Th>Header 2</Th>
-            <Th>Header 3</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td colSpan={1}>Row 1, Cell 1</Td>
-            <Td>Row 1, Cell 2</Td>
-            <Td>Row 1, Cell 3</Td>
-          </Tr>
-          <Tr>
-            <Td>Row 2, Cell 1</Td>
-            <Td>Row 2, Cell 2</Td>
-            <Td>Row 2, Cell 3</Td>
-          </Tr>
-          <Tr>
-            <Td>Row 3, Cell 1</Td>
-            <Td>Row 3, Cell 2</Td>
-            <Td>Row 3, Cell 3</Td>
-          </Tr>
-        </Tbody>
-      </Table>,
+      <Table.Root caption="Basic table">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCol>Header 1</Table.HeaderCol>
+            <Table.HeaderCol>Header 2</Table.HeaderCol>
+            <Table.HeaderCol>Header 3</Table.HeaderCol>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Row 1, Cell 1</Table.Cell>
+            <Table.Cell>Row 1, Cell 2</Table.Cell>
+            <Table.Cell>Row 1, Cell 3</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Row 2, Cell 1</Table.Cell>
+            <Table.Cell>Row 2, Cell 2</Table.Cell>
+            <Table.Cell>Row 2, Cell 3</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Row 3, Cell 1</Table.Cell>
+            <Table.Cell>Row 3, Cell 2</Table.Cell>
+            <Table.Cell>Row 3, Cell 3</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table.Root>,
     )
 
     expect(screen.getByRole('table')).toBeTruthy()
@@ -57,32 +57,34 @@ describe('Table', () => {
   test('should allow clickable headers', async () => {
     const handleClick = jest.fn()
     render(
-      <Table caption="Basic table">
-        <Thead>
-          <Tr>
-            <Th onClick={handleClick}>Header 1</Th>
-            <Th>Header 2</Th>
-            <Th>Header 3</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>Row 1, Cell 1</Td>
-            <Td>Row 1, Cell 2</Td>
-            <Td>Row 1, Cell 3</Td>
-          </Tr>
-          <Tr>
-            <Td>Row 2, Cell 1</Td>
-            <Td>Row 2, Cell 2</Td>
-            <Td>Row 2, Cell 3</Td>
-          </Tr>
-          <Tr>
-            <Td>Row 3, Cell 1</Td>
-            <Td>Row 3, Cell 2</Td>
-            <Td>Row 3, Cell 3</Td>
-          </Tr>
-        </Tbody>
-      </Table>,
+      <Table.Root caption="Basic table">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCol>
+              <Table.Trigger onClick={handleClick}>Header 1</Table.Trigger>
+            </Table.HeaderCol>
+            <Table.HeaderCol>Header 2</Table.HeaderCol>
+            <Table.HeaderCol>Header 3</Table.HeaderCol>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Row 1, Cell 1</Table.Cell>
+            <Table.Cell>Row 1, Cell 2</Table.Cell>
+            <Table.Cell>Row 1, Cell 3</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Row 2, Cell 1</Table.Cell>
+            <Table.Cell>Row 2, Cell 2</Table.Cell>
+            <Table.Cell>Row 2, Cell 3</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Row 3, Cell 1</Table.Cell>
+            <Table.Cell>Row 3, Cell 2</Table.Cell>
+            <Table.Cell>Row 3, Cell 3</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table.Root>,
     )
 
     expect(screen.getByText(/Header 1/i)).toBeTruthy()
@@ -91,5 +93,64 @@ describe('Table', () => {
 
     await userEvent.click(screen.getByText(/Header 1/i))
     expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+
+  test('should have a TableParts object', () => {
+    render(
+      <TableParts.Root>
+        <TableParts.Table>
+          <TableParts.Caption>Caption</TableParts.Caption>
+          <TableParts.Header>
+            <TableParts.Row>
+              <TableParts.HeaderCol>Header 1</TableParts.HeaderCol>
+              <TableParts.HeaderCol>Header 2</TableParts.HeaderCol>
+              <TableParts.HeaderCol>Header 3</TableParts.HeaderCol>
+            </TableParts.Row>
+          </TableParts.Header>
+          <TableParts.Body>
+            <TableParts.Row>
+              <TableParts.Cell>Row 1, Cell 1</TableParts.Cell>
+              <TableParts.Cell>Row 1, Cell 2</TableParts.Cell>
+              <TableParts.Cell>Row 1, Cell 3</TableParts.Cell>
+            </TableParts.Row>
+            <TableParts.Row>
+              <TableParts.Cell>Row 2, Cell 1</TableParts.Cell>
+              <TableParts.Cell>Row 2, Cell 2</TableParts.Cell>
+              <TableParts.Cell>Row 2, Cell 3</TableParts.Cell>
+            </TableParts.Row>
+            <TableParts.Row>
+              <TableParts.Cell>Row 3, Cell 1</TableParts.Cell>
+              <TableParts.Cell>Row 3, Cell 2</TableParts.Cell>
+              <TableParts.Cell>Row 3, Cell 3</TableParts.Cell>
+            </TableParts.Row>
+          </TableParts.Body>
+          <TableParts.Footer>
+            <TableParts.Row>
+              <TableParts.Cell>Footer 1</TableParts.Cell>
+              <TableParts.Cell>Footer 2</TableParts.Cell>
+              <TableParts.Cell>Footer 3</TableParts.Cell>
+            </TableParts.Row>
+          </TableParts.Footer>
+        </TableParts.Table>
+      </TableParts.Root>,
+    )
+
+    expect(screen.getByRole('table')).toBeTruthy()
+    expect(screen.getByRole('caption')).toBeTruthy()
+    expect(screen.getByText(/Header 1/i)).toBeTruthy()
+    expect(screen.getByText(/Header 2/i)).toBeTruthy()
+    expect(screen.getByText(/Header 3/i)).toBeTruthy()
+    expect(screen.getByText(/Row 1, Cell 1/i)).toBeTruthy()
+    expect(screen.getByText(/Row 1, Cell 2/i)).toBeTruthy()
+    expect(screen.getByText(/Row 1, Cell 3/i)).toBeTruthy()
+    expect(screen.getByText(/Row 2, Cell 1/i)).toBeTruthy()
+    expect(screen.getByText(/Row 2, Cell 2/i)).toBeTruthy()
+    expect(screen.getByText(/Row 2, Cell 3/i)).toBeTruthy()
+    expect(screen.getByText(/Row 3, Cell 1/i)).toBeTruthy()
+    expect(screen.getByText(/Row 3, Cell 2/i)).toBeTruthy()
+    expect(screen.getByText(/Row 3, Cell 3/i)).toBeTruthy()
+    expect(screen.getByText(/Footer 1/i)).toBeTruthy()
+    expect(screen.getByText(/Footer 2/i)).toBeTruthy()
+    expect(screen.getByText(/Footer 3/i)).toBeTruthy()
   })
 })
