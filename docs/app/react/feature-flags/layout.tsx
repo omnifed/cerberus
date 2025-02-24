a21deb78e45dca0e77d0cd03b93bea22da1da9da
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-02-24 12:12:34 -0600

    docs: update feature-flags page

---
+++ docs/app/react/feature-flags/layout.tsx
@@ -0,0 +1,24 @@
+import TwoTabLayout, {
+  type TwoTabLayoutProps,
+} from '@/app/components/page-tabs/two-tab-layout'
+import { type TwoTabs } from '@/components/page-tabs/primitives'
+import type { PropsWithChildren } from 'react'
+
+import { frontmatter } from './[slug]/overview.mdx'
+
+export interface RouteProps {
+  params: {
+    slug: TwoTabs
+  }
+}
+
+export default function Layout(props: PropsWithChildren<RouteProps>) {
+  return (
+    <TwoTabLayout
+      path="react/feature-flags"
+      meta={frontmatter as TwoTabLayoutProps['meta']}
+    >
+      {props.children}
+    </TwoTabLayout>
+  )
+}
