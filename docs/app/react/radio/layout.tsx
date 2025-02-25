df36795e2392a9aa89eba12a77b97cacca1dfc56
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-02-25 09:56:50 -0600

    docs: update radio pages

---
+++ docs/app/react/radio/layout.tsx
@@ -0,0 +1,24 @@
+import FourTabLayout, {
+  type FourTabLayoutProps,
+} from '@/app/components/page-tabs/four-tab-layout'
+import type { FourTabs } from '@/app/components/page-tabs/primitives'
+import type { PropsWithChildren } from 'react'
+
+import { frontmatter } from './[slug]/overview.mdx'
+
+export interface RouteProps {
+  params: {
+    slug: FourTabs
+  }
+}
+
+export default function Layout(props: PropsWithChildren<RouteProps>) {
+  return (
+    <FourTabLayout
+      path="radio"
+      meta={frontmatter as FourTabLayoutProps['meta']}
+    >
+      {props.children}
+    </FourTabLayout>
+  )
+}
