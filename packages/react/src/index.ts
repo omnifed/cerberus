31357f885083fbbe4857d5c0c883b74901838a6d
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-06-06 09:30:50 -0500

    chore: cache change tooltip file name

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -50,7 +50,6 @@ export * from './components/tabs/index'
 export * from './components/Tag'
 export * from './components/Text'
 export * from './components/toggle/index'
-export * from './components/Tooltip'
 
 // context
 
