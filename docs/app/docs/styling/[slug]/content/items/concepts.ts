4383fe9a94a6d020b066fd2ed66fb835d4adbdbf
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-06-12 15:29:42 -0500

    docs: add style concepts docs pt.1

--- docs/app/docs/styling/[slug]/content/items/concepts.ts
+++ docs/app/docs/styling/[slug]/content/items/concepts.ts
@@ -1,6 +1,16 @@
 import CerberusFactory, {
   frontmatter as cerberusFactoryFrontmatter,
 } from '../cerberus-factory.mdx'
+import ResponsiveDesign, {
+  frontmatter as responsiveDesignFrontmatter,
+} from '../responsive-design.mdx'
+import ConditionalStyles, {
+  frontmatter as conditionalStylesFrontmatter,
+} from '../conditional-styles.mdx'
+import CSSVars, { frontmatter as cssVarsFrontmatter } from '../css-vars.mdx'
+import ThemeMode, {
+  frontmatter as themeModeFrontmatter,
+} from '../theme-mode.mdx'
 
 export const concepts = [
   {
@@ -20,5 +30,57 @@ export const concepts = [
         'Learn about the Cerberus Factory, a powerful tool for managing styles and themes in the Cerberus Design System.',
     },
     Content: CerberusFactory,
+  },
+  {
+    id: '1.3',
+    label: 'Responsive Design',
+    slug: 'responsive-design',
+    frontmatter: responsiveDesignFrontmatter,
+    href: '/docs/styling/responsive-design',
+    meta: {
+      title: 'Responsive Design in Cerberus Design System',
+      description:
+        'Learn about Responsive Design and how to create responsive layouts using the Cerberus Design System.',
+    },
+    Content: ResponsiveDesign,
+  },
+  {
+    id: '1.4',
+    label: 'Conditional Styles',
+    slug: 'conditional-styles',
+    frontmatter: conditionalStylesFrontmatter,
+    href: '/docs/styling/conditional-styles',
+    meta: {
+      title: 'Conditional Styles in Cerberus Design System',
+      description:
+        'Learn how to apply conditional styles in Cerberus Design System',
+    },
+    Content: ConditionalStyles,
+  },
+  {
+    id: '1.5',
+    label: 'CSS Variables',
+    slug: 'css-vars',
+    frontmatter: cssVarsFrontmatter,
+    href: '/docs/styling/css-vars',
+    meta: {
+      title: 'CSS Variables in Cerberus Design System',
+      description:
+        'Learn how to use CSS Variables in Cerberus Design System for dynamic theming and styling.',
+    },
+    Content: CSSVars,
+  },
+  {
+    id: '1.6',
+    label: 'Themes & Color Modes',
+    slug: 'theme-mode',
+    frontmatter: themeModeFrontmatter,
+    href: '/docs/styling/theme-mode',
+    meta: {
+      title: 'Theme Mode in Cerberus Design System',
+      description:
+        'Learn how to implement and manage theme modes (light/dark) in Cerberus Design System.',
+    },
+    Content: ThemeMode,
   },
 ]
