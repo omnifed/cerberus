8bb22dea28bdb41e845512c9524e26560c15ae02
@casey_baggz_omni <casey.baggz@omnifederal.com>
2025-12-19 10:03:23 -0600

    docs: setup init PWA configs

--- packages/react/src/index.client.ts
+++ packages/react/src/index.client.ts
@@ -1,5 +1,22 @@
 'use client'
 
+// Unstyled Ark utilities
+
+export {
+  ClientOnly,
+  DownloadTrigger,
+  FocusTrap,
+  Format,
+  Frame,
+  Highlight,
+  JsonTreeView,
+  Presence,
+  EnvironmentProvider,
+  useEnvironmentContext,
+  LocaleProvider,
+  useLocaleContext,
+} from '@ark-ui/react'
+
 // 3rd party client exports
 
 export * from '@dnd-kit/core'
