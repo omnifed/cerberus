06391742d85074f61497e6b667461d0bb9372a4c
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-01-12 14:05:06 -0600

    fix(types): add missing symbol type for themes

--- packages/preset-acheron-theme/src/theme/index.ts
+++ packages/preset-acheron-theme/src/theme/index.ts
@@ -1,11 +1,12 @@
 import { defineTheme } from '@cerberus/tokens'
 import { semanticTokens } from './semantic-tokens/config'
+import { SemanticTokens } from '@pandacss/types'
 
 /**
  * This module contains the Acheron theme and configuration options.
  * @module
  */
 
-export const theme = defineTheme({
+export const theme: { semanticTokens: SemanticTokens } = defineTheme({
   semanticTokens,
 })
