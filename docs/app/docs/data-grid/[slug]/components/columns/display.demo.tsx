259e107b01ce838b4f44d0592690cb9a4b32dc46
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-02-18 15:21:11 -0600

    docs: create columns page

---
+++ docs/app/docs/data-grid/[slug]/components/columns/display.demo.tsx
@@ -0,0 +1,27 @@
+import {
+  type ColumnFeatures,
+  type DisplayColCellProps,
+} from '@cerberus-design/data-grid'
+import { ReactNode } from 'react'
+
+type DisplayOptions<TData> = {
+  id: string
+  header: string | ((props: { colId: string }) => ReactNode)
+  width?: number
+  features?: { pinning?: ColumnFeatures['pinning'] }
+  cell: (props: DisplayColCellProps<TData>) => ReactNode
+}
+
+// columnHelper.display({
+//   id: 'actions',
+//   header: '',
+//   width: 60,
+//   features: {
+//     pinning: 'right',
+//   },
+//   cell: () => (
+//     <IconButton ariaLabel="View more options">
+//       <OverflowMenuHorizontal />
+//     </IconButton>
+//   ),
+// })
