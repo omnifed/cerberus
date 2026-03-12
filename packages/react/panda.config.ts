1afce2e433ce3463b7957f790c65ce39270ea3df
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-03-12 14:43:34 -0500

    chore(react): update panda.config.ts

--- packages/react/panda.config.ts
+++ packages/react/panda.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
   preflight: true,
-  include: ['../../styled-system/**/*', './src/**/*.{ts,tsx}'],
+  include: ['./src/**/*.{ts,tsx}'],
 })
