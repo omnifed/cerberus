ab659ee6f99e401fe7c580fd1094f0bcca126e2a
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-04-23 10:10:14 -0500

    refactor(react): fix TS lint errors and deprecations for v7

--- packages/react/src/components/admonition/primitives.tsx
+++ packages/react/src/components/admonition/primitives.tsx
@@ -1,10 +1,5 @@
-import { type HTMLArkProps } from '@ark-ui/react/factory'
 import { admonition, type AdmonitionVariantProps } from 'styled-system/recipes'
-import {
-  cerberus,
-  createCerberusPrimitive,
-  type CerberusPrimitiveProps,
-} from '../../system/index'
+import { type CerberusProps, createCerberusPrimitive } from '../../system/index'
 
 /**
  * This module contains the primitives of the Admonition components.
@@ -16,62 +11,54 @@ const { withSlotRecipe, withNoRecipe } = createCerberusPrimitive(admonition)
 /**
  * The `AdmonitionRoot` component is the container for the Admonition.
  */
-export const AdmonitionRoot = withSlotRecipe(cerberus.aside, 'root', {
+export const AdmonitionRoot = withSlotRecipe('animateMotion', 'root', {
   defaultProps: {
     'data-scope': 'admonition',
     'data-part': 'root',
   },
 })
-export type AdmonitionRootProps = CerberusPrimitiveProps<
-  HTMLArkProps<'aside'> & AdmonitionVariantProps
->
+export type AdmonitionRootProps = CerberusProps<'aside'> & AdmonitionVariantProps
 
 /**
  * The `AdmonitionIndicator` component is the indicator for the Admonition.
  */
-export const AdmonitionIndicator = withNoRecipe(cerberus.span, {
+export const AdmonitionIndicator = withNoRecipe('span', {
   defaultProps: {
     'data-scope': 'admonition',
     'data-part': 'indicator',
   },
 })
-export type AdmonitionIndicatorProps = CerberusPrimitiveProps<
-  HTMLArkProps<'span'>
->
+export type AdmonitionIndicatorProps = CerberusProps<'span'>
 
 /**
  * The `AdmonitionContent` component is the content for the Admonition.
  */
-export const AdmonitionContent = withNoRecipe(cerberus.div, {
+export const AdmonitionContent = withNoRecipe('div', {
   defaultProps: {
     'data-scope': 'admonition',
     'data-part': 'content',
   },
 })
-export type AdmonitionContentProps = CerberusPrimitiveProps<HTMLArkProps<'div'>>
+export type AdmonitionContentProps = CerberusProps<'div'>
 
 /**
  * The `AdmonitionHeading` component is the heading title for the Admonition.
  */
-export const AdmonitionHeading = withSlotRecipe(cerberus.p, 'heading', {
+export const AdmonitionHeading = withSlotRecipe('p', 'heading', {
   defaultProps: {
     'data-scope': 'admonition',
     'data-part': 'heading',
   },
 })
-export type AdmonitionHeadingProps = CerberusPrimitiveProps<
-  HTMLArkProps<'p'> & AdmonitionVariantProps
->
+export type AdmonitionHeadingProps = CerberusProps<'p'> & AdmonitionVariantProps
 
 /**
  * The `AdmonitionDescription` component is the description for the Admonition.
  */
-export const AdmonitionDescription = withSlotRecipe(cerberus.p, 'description', {
+export const AdmonitionDescription = withSlotRecipe('p', 'description', {
   defaultProps: {
     'data-scope': 'admonition',
     'data-part': 'description',
   },
 })
-export type AdmonitionDescriptionProps = CerberusPrimitiveProps<
-  HTMLArkProps<'p'> & AdmonitionVariantProps
->
+export type AdmonitionDescriptionProps = CerberusProps<'p'> & AdmonitionVariantProps
