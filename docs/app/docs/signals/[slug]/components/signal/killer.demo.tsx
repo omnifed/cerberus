ebef06437a67b5fca9b69692bd5996556f41a701
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-04-23 16:24:19 -0500

    docs: update signal docs

---
+++ docs/app/docs/signals/[slug]/components/signal/killer.demo.tsx
@@ -0,0 +1,9 @@
+import { useSignal } from '@cerberus-design/signals'
+
+const [_local, _setLocal, getLocal] = useSignal('hello')
+
+// Even if this runs 10 minutes from now, it will ALWAYS have the
+// absolute latest state, with zero dependency arrays needed.
+setTimeout(() => {
+  console.log(getLocal())
+}, 600000)
