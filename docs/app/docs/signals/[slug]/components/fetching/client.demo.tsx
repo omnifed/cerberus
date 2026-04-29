549823b0e701b7bf1c86448b66bcc33433e6e116
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-04-29 12:31:45 -0500

    chore: fix docs lint error

--- docs/app/docs/signals/[slug]/components/fetching/client.demo.tsx
+++ docs/app/docs/signals/[slug]/components/fetching/client.demo.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useQuery } from '@cerberus-design/signals'
-import { queryUser, type User } from './queries'
+import { queryUser } from './queries'
+import { type User } from './db'
 
 interface Props {
   id: string
@@ -12,6 +13,8 @@ export function UserProfile(props: Props) {
   // 1. Cerberus sees `initialData`, instantly seeds the $O(1) Map,
   // and mounts the component with zero loading spinners or network waterfalls.
   const user = useQuery(queryUser(props.id), { initialData: props.initialData })
+
+  if (!user) return null
 
   return <h1>{user.name}</h1>
 }
