3312fe811c74bfc03742922aa3ac2c499d4c2ce6
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-04-29 11:45:11 -0500

    docs: update query hook pages

--- docs/app/docs/signals/[slug]/components/fetching/queries.ts
+++ docs/app/docs/signals/[slug]/components/fetching/queries.ts
@@ -1,32 +1,12 @@
 // Queries
 
 import { createQuery } from '@cerberus-design/signals'
+import { User, api } from './db'
 
 export const queryUser = createQuery(getUser, 'queryUser')
 
 // API
 
-async function getUser(id: string): Promise<User> {
+async function getUser(id: string): Promise<User | null> {
   return await api.getUser(id)
-}
-
-// Fake DB
-
-export type User = {
-  id: string
-  name: string
-}
-
-const users = new Map<User['id'], User>()
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-
-const api = {
-  getUser: async (id: string): Promise<User> => {
-    await delay(800)
-    const user = users.get(id)
-    if (user) return user
-    const newUser = await getUser(id)
-    users.set(newUser.id, newUser)
-    return newUser
-  },
 }
