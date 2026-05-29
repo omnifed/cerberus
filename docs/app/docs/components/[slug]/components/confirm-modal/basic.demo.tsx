40658670922e4d12972ca607748a4338167cea3f
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-05-29 14:08:44 -0500

    docs: update confirm page

---
+++ docs/app/docs/components/[slug]/components/confirm-modal/basic.demo.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { HStack } from '@/styled-system/jsx'
+import {
+  Button,
+  ConfirmModal,
+  Show,
+  Text,
+  useConfirmModal,
+} from '@cerberus-design/react'
+import { useSignal } from '@cerberus-design/signals'
+import Link from 'next/link'
+
+export function BasicDemo() {
+  return (
+    <ConfirmModal>
+      <NonDestructiveFeature />
+    </ConfirmModal>
+  )
+}
+
+function NonDestructiveFeature() {
+  const confirm = useConfirmModal()
+  const [consent, setConsent] = useSignal<boolean | null>(null)
+
+  const handleClick = async () => {
+    const userConsent = await confirm.show({
+      heading: 'Add new payment method?',
+      description: (
+        <>
+          This will add a new payment method to your account to be billed for future
+          purchases. <Link href="#">Learn more</Link>.
+        </>
+      ),
+      actionText: 'Yes, add payment method',
+      cancelText: 'No, cancel',
+    })
+    setConsent(userConsent)
+  }
+
+  return (
+    <HStack gap="md">
+      <HStack gap="md">
+        <Button onClick={handleClick}>Confirm</Button>
+      </HStack>
+
+      <Show when={consent !== null}>
+        <Text as="small" textStyle="label-sm">
+          User consent:{' '}
+          <Text as="span" fontSize="xs" textStyle="mono-xs">
+            {JSON.stringify(consent)}
+          </Text>
+        </Text>
+      </Show>
+    </HStack>
+  )
+}
