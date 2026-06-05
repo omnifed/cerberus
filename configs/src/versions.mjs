4374e56ce44b5b38b2f1c02e361eb53287498074
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-06-05 10:29:54 -0500

    ci: add publish scripts

--- configs/src/versions.mjs
+++ configs/src/versions.mjs
@@ -1,14 +1,13 @@
 import { version as currentVersion } from '../../packages/data-grid/package.json'
 
 export const version = currentVersion
-// export const nextTag = 'next'
 
 // Order matters
-// export const packages = [
-//   'figma',
-//   'tokens',
-//   'preset-acheron-theme',
-//   'preset-cerberus-theme',
-//   'preset-elysium-theme',
-//   'panda-preset',
-// ]
+export const jsrPackages = [
+  'figma',
+  'tokens',
+  'preset-acheron-theme',
+  'preset-cerberus-theme',
+  'preset-elysium-theme',
+  'panda-preset',
+]
