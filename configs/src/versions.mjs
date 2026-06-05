b138f0537e8b80c513215a791f3e4554aef5902c
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-06-05 10:21:56 -0500

    docs: remove dependency on versions.mjs

--- configs/src/versions.mjs
+++ configs/src/versions.mjs
@@ -1,14 +1,14 @@
 import { version as currentVersion } from '../../packages/data-grid/package.json'
 
 export const version = currentVersion
-export const nextTag = 'next'
+// export const nextTag = 'next'
 
 // Order matters
-export const packages = [
-  'figma',
-  'tokens',
-  'preset-acheron-theme',
-  'preset-cerberus-theme',
-  'preset-elysium-theme',
-  'panda-preset',
-]
+// export const packages = [
+//   'figma',
+//   'tokens',
+//   'preset-acheron-theme',
+//   'preset-cerberus-theme',
+//   'preset-elysium-theme',
+//   'panda-preset',
+// ]
