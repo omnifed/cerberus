0bb5a4b6fa2fd38c17c02b3966015bad6c44fedf
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-06-08 15:06:53 -0500

    docs: add init visibity page to data-grid section

---
+++ docs/app/docs/data-grid/[slug]/components/visibility/static.tsx
@@ -0,0 +1,16 @@
+export const DEMOS = {
+  basic: {
+    id: 'visibility.basic',
+    preview: null,
+    context: 'data-grid',
+  },
+  hidden: {
+    id: 'visibility.hidden',
+    preview: null,
+    context: 'data-grid',
+  },
+  def: {
+    id: 'visibility.def',
+    context: 'data-grid',
+  },
+}
