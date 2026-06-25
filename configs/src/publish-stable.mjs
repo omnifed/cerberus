59ac7baf041caff4493595ae694c0d1386537175
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-06-25 12:20:52 -0500

    ci: revert publish-stable.mjs

--- configs/src/publish-stable.mjs
+++ configs/src/publish-stable.mjs
@@ -8,7 +8,9 @@ const repoRoot = resolve(import.meta.dir, '..', '..')
 console.log('🧪 Publishing NPM Stable Packages in dependant order...')
 for (const pkg of npmPackages) {
   console.log(`Publishing @cerberus-design/${pkg}...`)
-  await $`pnpm --filter "@cerberus-design/${pkg}" build && pnpm --filter "@cerberus-design/${pkg}" publish --tag latest --access public --no-git-checks`.cwd(repoRoot)
+  await $`cd packages/${pkg} && pnpm build && pnpm publish --tag next --access public --no-git-checks`.cwd(
+    repoRoot,
+  )
 }
 
 console.log('🧪 Publishing JSR Canary Packages in dependant order...')
