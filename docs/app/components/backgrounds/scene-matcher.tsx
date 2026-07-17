a4f7a373dda094ed5f1fdbdb7c65834c6dccaf41
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-07-17 11:40:40 -0500

    docs: add cerberus and oceanus day scenes

--- docs/app/components/backgrounds/scene-matcher.tsx
+++ docs/app/components/backgrounds/scene-matcher.tsx
@@ -2,11 +2,13 @@
 
 import { useThemeContext } from '@cerberus-design/react'
 import AcheronBackground from './acheron'
-import CerberusBackground from './cerberus'
-import ElysiumBackground from './elysium'
-import OceanusBackground from './oceanus'
 import AcheronDayBackground from './acheron-day'
+import CerberusBackground from './cerberus'
+import CerberusDayBackground from './cerberus-day'
+import ElysiumBackground from './elysium'
 import ElysiumDayBackground from './elysium-day'
+import OceanusBackground from './oceanus'
+import OceanusDayBackground from './oceanus-day'
 
 export function SceneMatcher() {
   const { theme, mode } = useThemeContext()
@@ -16,9 +18,9 @@ export function SceneMatcher() {
   if (mode === 'light') {
     switch (theme) {
       case 'oceanus':
-        return <OceanusBackground />
+        return <OceanusDayBackground />
       case 'cerberus':
-        return <CerberusBackground />
+        return <CerberusDayBackground />
       case 'elysium':
         return <ElysiumDayBackground />
       case 'acheron':
