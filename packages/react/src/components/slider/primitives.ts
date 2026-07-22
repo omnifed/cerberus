03f7de57b4dff0a8edc1779f5fc1d38dd03fa5df
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-07-22 10:31:03 -0500

    feat(react): add slider primitives

--- packages/react/src/components/slider/primitives.ts
+++ packages/react/src/components/slider/primitives.ts
@@ -10,9 +10,56 @@ import {
  * @module 'slider/primitives'
  */
 
-const { withSlotRecipe } = createCerberusPrimitive(slider)
+const { withSlotRecipe, withNoRecipe } = createCerberusPrimitive(slider)
+
+// Root
 
 export type SliderRootProps = CerberusPrimitiveProps<
   Slider.RootProps & SliderVariantProps
 >
 export const SliderRoot = withSlotRecipe(Slider.Root, 'root')
+
+// Label
+
+export type SliderLabelProps = CerberusPrimitiveProps<Slider.LabelProps>
+export const SliderLabel = withSlotRecipe(Slider.Label, 'label')
+
+// ValueText
+
+export type SliderValueTextProps = CerberusPrimitiveProps<Slider.ValueTextProps>
+export const SliderValueText = withSlotRecipe(Slider.ValueText, 'valueText')
+
+// Control
+
+export type SliderControlProps = CerberusPrimitiveProps<Slider.ControlProps>
+export const SliderControl = withSlotRecipe(Slider.Control, 'control')
+
+// Track
+
+export type SliderTrackProps = CerberusPrimitiveProps<Slider.TrackProps>
+export const SliderTrack = withSlotRecipe(Slider.Track, 'track')
+
+// Range
+
+export type SliderRangeProps = CerberusPrimitiveProps<Slider.RangeProps>
+export const SliderRange = withSlotRecipe(Slider.Range, 'range')
+
+// Thumb
+
+export type SliderThumbProps = CerberusPrimitiveProps<Slider.ThumbProps>
+export const SliderThumb = withSlotRecipe(Slider.Thumb, 'thumb')
+
+// MarkerGroup
+
+export type SliderMarkerGroupProps = CerberusPrimitiveProps<Slider.MarkerGroupProps>
+export const SliderMarkerGroup = withSlotRecipe(Slider.MarkerGroup, 'markerGroup')
+
+// Marker
+
+export type SliderMarkerProps = CerberusPrimitiveProps<Slider.MarkerProps>
+export const SliderMarker = withSlotRecipe(Slider.Marker, 'marker')
+
+// HiddenInput
+
+export type SliderHiddenInputProps = CerberusPrimitiveProps<Slider.HiddenInputProps>
+export const SliderHiddenInput = withNoRecipe(Slider.HiddenInput)
