5a612ba2ccfcddcfad5cd7ee108b75e8fa5526c8
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-07-22 11:40:44 -0500

    feat(preset): add initial slider styles to recipe

--- packages/panda-preset/src/recipes/slots/slider.ts
+++ packages/panda-preset/src/recipes/slots/slider.ts
@@ -29,9 +29,193 @@ export const slider: Partial<SlotRecipeConfig> = defineSlotRecipe({
     'Slider',
   ],
 
-  base: {},
+  base: {
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1',
+      isolation: 'isolate',
+      position: 'relative',
+      textStyle: 'body-sm',
+      touchAction: 'none',
+    },
+    label: {
+      textStyle: 'label-sm',
+    },
+    control: {
+      alignItems: 'center',
+      display: 'inline-flex',
+      position: 'relative',
+    },
+    track: {
+      borderRadius: 'full',
+      flex: '1',
+      overflow: 'hidden',
+    },
+    range: {
+      height: 'inherit',
+      width: 'inherit',
+      _disabled: { bg: 'border.emphasized!' },
+    },
+    markerGroup: {
+      position: 'absolute!',
+      zIndex: '1',
+    },
+    marker: {
+      '--marker-bg': { base: 'white', _underValue: 'colors.bg' },
+      alignItems: 'center',
+      color: 'fg.muted',
+      display: 'flex',
+      gap: 'calc(var(--slider-thumb-size) / 2)',
+      textStyle: 'body-xs',
+    },
+    draggingIndicator: {
+      bgColor: 'var(--marker-bg)',
+      borderRadius: 'full',
+      height: 'calc(var(--slider-track-size) - var(--slider-marker-size))',
+      width: 'calc(var(--slider-track-size) - var(--slider-marker-size))',
+    },
+    thumb: {
+      alignItems: 'center',
+      borderRadius: 'full',
+      display: 'flex',
+      height: 'var(--slider-thumb-size)',
+      justifyContent: 'center',
+      outline: 0,
+      transition: 'shadow',
+      width: 'var(--slider-thumb-size)',
+      zIndex: '2',
+      _focusVisible: {
+        ring: '3px',
+        ringColor: 'action.border.focus',
+      },
+    },
+  },
 
-  variants: {},
+  variants: {
+    size: {
+      sm: {
+        root: {
+          '--slider-thumb-size': '1.5rem',
+          '--slider-track-size': '0.5rem',
+          '--slider-marker-center': '6px',
+          '--slider-marker-size': '0.06rem',
+          '--slider-marker-inset': '3px',
+        },
+      },
+      md: {
+        root: {
+          '--slider-thumb-size': '1.75rem',
+          '--slider-track-size': '0.75rem',
+          '--slider-marker-center': '7px',
+          '--slider-marker-size': '0.12rem',
+          '--slider-marker-inset': '4px',
+        },
+      },
+      lg: {
+        root: {
+          '--slider-thumb-size': '2rem',
+          '--slider-track-size': '1rem',
+          '--slider-marker-center': '9px',
+          '--slider-marker-size': '0.19rem',
+          '--slider-marker-inset': '5px',
+        },
+      },
+    },
+    usage: {
+      filled: {
+        track: {
+          bgColor: 'colorPalette.surface.300',
+          _disabled: {
+            opacity: '0.5',
+          },
+        },
+        range: {
+          bgColor: 'colorPalette.bg.initial',
+        },
+        thumb: {
+          bg: 'action.ghost.initial',
+          _disabled: {
+            bg: 'border.emphasized',
+          },
+        },
+      },
+      outlined: {
+        track: {
+          bgColor: 'colorPalette.surface.300',
+          shadow: 'inset',
+        },
+        range: {
+          gradient: 'charon-dark',
+        },
+        thumb: {
+          borderWidth: '2px',
+          borderColor: 'action.border.initial',
+          bgColor: 'colorPalette.bg.100',
+          _disabled: {
+            opacity: '0.5',
+          },
+        },
+      },
+    },
+    orientation: {
+      vertical: {
+        root: {
+          display: 'inline-flex',
+        },
+        control: {
+          flexDirection: 'column',
+          height: '100%',
+          minWidth: 'var(--slider-thumb-size)',
+          '&:has(.chakra-slider__markerLabel)': {
+            marginEnd: 'md',
+          },
+        },
+        track: {
+          width: 'var(--slider-track-size)',
+        },
+        thumb: {
+          left: '50%',
+          translate: '-50% 0',
+        },
+        markerGroup: {
+          insetStart: 'var(--slider-marker-center)',
+          insetBlock: 'var(--slider-marker-inset)',
+        },
+        marker: {
+          flexDirection: 'row',
+        },
+      },
+      horizontal: {
+        control: {
+          flexDirection: 'row',
+          width: '100%',
+          minHeight: 'var(--slider-thumb-size)',
+          '&:has(.chakra-slider__markerLabel)': {
+            marginBottom: 'md',
+          },
+        },
+        track: {
+          height: 'var(--slider-track-size)',
+        },
+        thumb: {
+          top: '50%',
+          translate: '0 -50%',
+        },
+        markerGroup: {
+          top: 'var(--slider-marker-center)',
+          insetInline: 'var(--slider-marker-inset)',
+        },
+        marker: {
+          flexDirection: 'column',
+        },
+      },
+    },
+  },
 
-  defaultVariants: {},
+  defaultVariants: {
+    size: 'lg',
+    orientation: 'horizontal',
+    usage: 'filled',
+  },
 })
--- packages/react/src/components/slider/slider.tsx
+++ packages/react/src/components/slider/slider.tsx
@@ -1,3 +1,38 @@
+import { SliderRootProvider } from '@ark-ui/react'
+import {
+  SliderLabel,
+  SliderRoot,
+  SliderControl,
+  SliderTrack,
+  SliderRange,
+  SliderThumb,
+  SliderMarkerGroup,
+  SliderMarker,
+  SliderHiddenInput,
+} from './primitives'
+
+/**
+ * ## Slider Component
+ * Slider component that wraps the slider primitives. This provides an abstraction to streamline basic slider usage.
+ *
+ * For more advanced use cases, use the slider
+ * primitives directly via the `Slider.[Slot]` component's parts.
+ *
+ * @see {@link https://cerberus.digitalu.design/docs/components/slider}
+ */
 export function Slider() {
   return <div role="slider" />
 }
+
+// Parts
+
+Slider.Provider = SliderRootProvider
+Slider.Root = SliderRoot
+Slider.Label = SliderLabel
+Slider.Control = SliderControl
+Slider.Track = SliderTrack
+Slider.Range = SliderRange
+Slider.Thumb = SliderThumb
+Slider.MarkerGroup = SliderMarkerGroup
+Slider.Marker = SliderMarker
+Slider.HiddenInput = SliderHiddenInput
