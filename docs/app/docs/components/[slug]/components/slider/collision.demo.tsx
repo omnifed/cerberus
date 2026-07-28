d38eeff1326c08d0d964c1e21bfe7f03c689aca4
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-07-28 11:17:51 -0500

    docs: update slider collision to show all variants

--- docs/app/docs/components/[slug]/components/slider/collision.demo.tsx
+++ docs/app/docs/components/[slug]/components/slider/collision.demo.tsx
@@ -1,10 +1,20 @@
-import { HStack } from '@/styled-system/jsx'
+import { Stack } from '@/styled-system/jsx'
 import { Slider } from '@cerberus-design/react'
 
 export function CollisionDemo() {
   return (
-    <HStack w="3/4">
-      <Slider defaultValue={[30, 80]} thumbCollisionBehavior="push" />
-    </HStack>
+    <Stack gap="lg" w="3/4">
+      <Slider defaultValue={[30, 80]} thumbCollisionBehavior="none">
+        <Slider.Label>None</Slider.Label>
+      </Slider>
+
+      <Slider defaultValue={[30, 80]} thumbCollisionBehavior="push">
+        <Slider.Label>Push</Slider.Label>
+      </Slider>
+
+      <Slider defaultValue={[30, 80]} thumbCollisionBehavior="swap">
+        <Slider.Label>Swap</Slider.Label>
+      </Slider>
+    </Stack>
   )
 }
