34d24b3f93f929d85d51774049f960bbf8383704
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-08-27 12:04:29 -0500

    docs: relocate button demos to examples

--- docs/examples/components/button/index.tsx
+++ docs/examples/components/button/index.tsx
@@ -10,43 +10,33 @@ import { CustomDemo } from './custom.demo'
 
 export const DEMOS = {
   basic: {
-    id: 'button.basic',
     preview: <BasicDemo />,
   },
   icon: {
-    id: 'button.icon',
     preview: <IconDemo />,
   },
   pending: {
-    id: 'button.pending',
     preview: <PendingDemo />,
   },
   shapes: {
-    id: 'button.shapes',
     preview: <ShapesDemo />,
   },
   usage: {
-    id: 'button.usage',
     preview: <UsageDemo />,
   },
   size: {
-    id: 'button.size',
     preview: <SizeDemo />,
   },
   group: {
-    id: 'button.group',
     preview: <GroupDemo />,
   },
   attached: {
-    id: 'button.attached',
     preview: <AttachedDemo />,
   },
   custom: {
-    id: 'button.custom',
     preview: <CustomDemo />,
   },
   global: {
-    id: 'button.global',
+    preview: null,
   },
-  meta: `import { Button } from '@cerberus/react';`,
 }
